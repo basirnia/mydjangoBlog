b8dee5e2f08ba3ae921e2f5b4b4b6dfde57d3b6a
Basirnia <Basirnia@yahoo.com>
2021-09-24 02:43:31 +0330

    session 14

--- articles/models.py
+++ articles/models.py
@@ -7,6 +7,7 @@ class Article(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(default='default.jpg', blank=True)
     # in add thumbnail
     # add in author
 
