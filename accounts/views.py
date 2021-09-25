d4338c8cd34064abd41f2342eb8662c544682429
Basirnia <Basirnia@yahoo.com>
2021-09-25 03:45:12 +0330

    session 16

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth import forms
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms  import UserCreationForm
 
 def signup_view(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if  form.is_valid():
+            form.save()
+            # login
+            return redirect('articles:list')
+    else:
+        form = UserCreationForm()
     args = {'form':form}
-    return render(request,'accounts/signup.html', args)
+    return render(request,'accounts/signup.html',args)
 
 # Create your views here.
---
+++ runserver.bat
@@ -0,0 +1,3 @@
+@echo off
+title runserver
+start cmd /k "c: && cd C:\Users\Basirnia\Desktop\DjangoBlog\envs\Scripts && activate && cd C:\Users\Basirnia\Desktop\DjangoBlog && python manage.py runserver "
